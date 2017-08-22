6a42152ef6312e7429a9e8f897dcb17d363cc7a4
Vadim Aleksandrov <valeksandrov@me.com>
2017-08-22 18:58:53 +0300

    Some additional change

--- plex2myshows/modules/myshows/exceptions.py
+++ plex2myshows/modules/myshows/exceptions.py
@@ -2,25 +2,9 @@ class MyshowsAPIError(Exception):
     """Root exception for all errors related to this library"""
 
 
-class TransportError(MyshowsAPIError):
-    """An error occurred while performing a connection to the server"""
-
-
-class ProtocolError(MyshowsAPIError):
-    """An error occurred while dealing with the JSON-RPC protocol"""
+class APIError(MyshowsAPIError):
+    """An error occurred while performing a request to API"""
 
 
 class AuthError(MyshowsAPIError):
-    """An error occurred while performing authorization with OAuth2"""
-
-
-class MyshowsOAuth2CommonError(MyshowsAPIError):
-    """An error occured while use token cache file or something else"""
-
-
-class MyshowsOAuth2CodeError(MyshowsAPIError):
-    """An error occured while get OAuth2 token with authorization code"""
-
-
-class MyshowsOAuth2TokenError(MyshowsAPIError):
-    """An error occured with OAuth2 token"""
+    """An error occurred while performing authorization"""
